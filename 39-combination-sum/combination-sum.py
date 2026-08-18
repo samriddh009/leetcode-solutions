5380bbae9dde7f0da89bada4043bf7dabaf5d7f4
Samriddh Gupta <samriddhgupta009@gmail.com>
2026-08-18 16:30:02 +0530

    Time: 9 ms (53.77%) | Memory: 19.3 MB (91.54%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,22 +1,44 @@
 #input output approach 
-def fun(nums,temp,cur_sum,ans,target,d):
+# def fun(nums,temp,cur_sum,ans,target,d):
+#     if cur_sum == target:
+#         k = tuple(sorted(temp))
+#         if k not in d:
+#             d[k]=1
+#             ans.append(temp)
+#         return 
+#     if cur_sum>target:
+#         return 
+#     if not nums:
+#         return 
+#     for i in range(len(nums)):
+#         op = temp +[nums[i]]
+#         fun(nums,op,cur_sum+nums[i],ans,target,d)
+#     return ans
+#class Solution:
+    #def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # ans = []
+        # d = {}
+        # fun(candidates,[],0,ans,target,d)
+        # return ans
+
+#take not take
+def fun(i,nums,op,cur_sum,target,ans,d):
     if cur_sum == target:
-        k = tuple(sorted(temp))
+        k = tuple(sorted(op))
         if k not in d:
             d[k]=1
-            ans.append(temp)
+            ans.append(op.copy())
         return 
-    if cur_sum>target:
-        return 
-    if not nums:
-        return 
-    for i in range(len(nums)):
-        op = temp +[nums[i]]
-        fun(nums,op,cur_sum+nums[i],ans,target,d)
-    return ans
+    if cur_sum>target or i>=len(nums):
+        return
+    op.append(nums[i])
+    fun(i, nums, op, cur_sum + nums[i], target, ans, d)
+    op.pop()
+    fun(i+1, nums, op, cur_sum, target, ans, d)
+
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
         d = {}
-        fun(candidates,[],0,ans,target,d)
+        fun(0,candidates,[],0,target,ans,d)
         return ans
