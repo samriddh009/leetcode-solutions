9832f95bf7a88d7c966fb6406b4fd7f55a7245fd
Samriddh Gupta <samriddhgupta009@gmail.com>
2026-08-19 16:11:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (60.03%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,13 +1,11 @@
 #input output approach 
 def fun(nums,temp,ans):
-    d = {}
     ans.append(temp)
     if not nums:
         return 
     for i in range(len(nums)):
-        if nums[i] in d:
+        if i>0 and nums[i] ==nums[i-1] :
             continue
-        d[nums[i]] = 1
         ip = nums.copy()
         op = temp +[nums[i]]
         del ip[:i+1]
@@ -15,11 +13,11 @@ def fun(nums,temp,ans):
     return ans
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
         # res = [[]]
         # for i in nums:
         #     res += [curr + [i] for curr in res]
         # return list(set(tuple(x) for x in res))
+        nums.sort()
         ans = []
         fun(nums,[],ans)
         return ans
