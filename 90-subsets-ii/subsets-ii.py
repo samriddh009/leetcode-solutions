351df3db17c295a91f221493cb193cee81bee042
Samriddh Gupta <samriddhgupta009@gmail.com>
2026-08-25 22:54:09 +0530

    Time: 1 ms (27.37%) | Memory: 19.4 MB (60.38%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -13,10 +13,6 @@ def fun(nums,temp,ans):
     return ans
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        # res = [[]]
-        # for i in nums:
-        #     res += [curr + [i] for curr in res]
-        # return list(set(tuple(x) for x in res))
         nums.sort()
         ans = []
         fun(nums,[],ans)
