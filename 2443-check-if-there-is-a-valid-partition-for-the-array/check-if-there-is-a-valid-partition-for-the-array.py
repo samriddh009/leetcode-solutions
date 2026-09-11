4e1fa034479d6a5f0d74fc4ff8199818f1e0deed
Samriddh Gupta <samriddhgupta009@gmail.com>
2026-09-11 19:13:51 +0530

    Time: 199 ms (24.62%) | Memory: 139.7 MB (5.30%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.py
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,20 +1,32 @@
-@cache
-def fun(i,nums):
-    n = len(nums)
-    if i == n:
-        return True
-    if i + 2 <= n:
-        if nums[i + 1] == nums[i]:
-            if fun(i + 2,nums):
-                return True
-    if i + 3 <= n:
-        if nums[i + 2] == nums[i + 1] and nums[i + 1] == nums[i]:
-            if fun(i + 3,nums):
-                return True
-        if nums[i + 2] == nums[i + 1] + 1 and nums[i + 1] == nums[i] + 1:
-            if fun(i + 3,nums):
-                return True
-    return False
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
-        return fun(0,tuple(nums))
+        n = len(nums)
+        @cache
+        def fun(i: int) -> bool:
+            if i == n - 1:
+                return True
+            next_2 = i + 2
+            if next_2 < n:
+                if nums[next_2] == nums[next_2 - 1]:
+                    if fun(next_2):
+                        return True
+            next_3 = i + 3
+            if next_3 < n:
+                if nums[next_3] == nums[next_3 - 1] and nums[next_3] == nums[next_3 - 2]:
+                    if fun(next_3):
+                        return True
+                if nums[next_3] == nums[next_3 - 1] + 1 and nums[next_3 - 1] == nums[next_3 - 2] + 1:
+                    if fun(next_3):
+                        return True
+            return False
+        if n >= 2 and nums[1] == nums[0]:
+            if fun(1):
+                return True
+        if n >= 3:
+            if nums[2] == nums[1] and nums[2] == nums[0]:
+                if fun(2):
+                    return True
+            if nums[2] == nums[1] + 1 and nums[1] == nums[0] + 1:
+                if fun(2):
+                    return True
+        return False
