f2437cd4a3fee7dbf2ab9faa1e17414ad848da89
Samriddh Gupta <samriddhgupta009@gmail.com>
2026-09-11 19:25:05 +0530

    Time: 172 ms (46.59%) | Memory: 138.3 MB (7.20%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.py
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.py
@@ -2,31 +2,21 @@ class Solution:
     def validPartition(self, nums: List[int]) -> bool:
         n = len(nums)
         @cache
-        def fun(i: int) -> bool:
+        def fun(i):
             if i == n - 1:
                 return True
-            next_2 = i + 2
-            if next_2 < n:
-                if nums[next_2] == nums[next_2 - 1]:
-                    if fun(next_2):
+            l = i + 2
+            if l < n:
+                if nums[l] == nums[l - 1]:
+                    if fun(l):
                         return True
-            next_3 = i + 3
-            if next_3 < n:
-                if nums[next_3] == nums[next_3 - 1] and nums[next_3] == nums[next_3 - 2]:
-                    if fun(next_3):
+            l = i + 3
+            if l < n:
+                if nums[l] == nums[l - 1] and nums[l] == nums[l - 2]:
+                    if fun(l):
                         return True
-                if nums[next_3] == nums[next_3 - 1] + 1 and nums[next_3 - 1] == nums[next_3 - 2] + 1:
-                    if fun(next_3):
+                if nums[l] == nums[l - 1] + 1 and nums[l - 1] == nums[l - 2] + 1:
+                    if fun(l):
                         return True
             return False
-        if n >= 2 and nums[1] == nums[0]:
-            if fun(1):
-                return True
-        if n >= 3:
-            if nums[2] == nums[1] and nums[2] == nums[0]:
-                if fun(2):
-                    return True
-            if nums[2] == nums[1] + 1 and nums[1] == nums[0] + 1:
-                if fun(2):
-                    return True
-        return False
+        return fun(-1)
